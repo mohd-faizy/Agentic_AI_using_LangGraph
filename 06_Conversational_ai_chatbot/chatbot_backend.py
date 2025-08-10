5e59f697add720b0abd452a952ca10a991265223
MOHD FAIZY <ifaizymohd@gmail.com>
2025-08-10 19:30:34 +0530

    update

--- 06_Conversational_ai_chatbot/chatbot_backend.py
+++ 06_Conversational_ai_chatbot/chatbot_backend.py
@@ -1,43 +1,43 @@
-# Import required classes and functions for LangGraph
+
+
 from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
-from langchain_core.messages import BaseMessage
+from langchain_core.messages import BaseMessage, HumanMessage
 from langchain_groq import ChatGroq
-from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.checkpoint.sqlite import SqliteSaver
 from langgraph.graph.message import add_messages
 from dotenv import load_dotenv
+import sqlite3
 
-# Load environment variables from a .env file
 load_dotenv()
 
-# Initialize language model 
 llm = ChatGroq(model="llama-3.1-8b-instant")
 
-# Define a shared state structure for the LangGraph pipeline
 class ChatState(TypedDict):
-    messages: Annotated[list[BaseMessage], add_messages]  # messages will be persisted and passed between nodes
+    messages: Annotated[list[BaseMessage], add_messages]
 
-# Define a LangGraph node function to process chat messages
 def chat_node(state: ChatState):
-    messages = state["messages"]     # Extract previous messages from state
-    response = llm.invoke(messages)  # Generate response using the LLM
-    return {"messages": [response]}  # Return the new message to update the state
+    messages = state['messages']
+    response = llm.invoke(messages)
+    return {"messages": [response]}
 
-# Initialize an in-memory checkpointer to store intermediate state
-checkpointer = InMemorySaver()
+conn = sqlite3.connect(database='chatbot.db', check_same_thread=False)
+# Checkpointer
+checkpointer = SqliteSaver(conn=conn)
 
-# Create the LangGraph stateful graph
-graph = StateGraph(ChatState)  # Pass the expected state type here
-
-# Add a node 
+graph = StateGraph(ChatState)
 graph.add_node("chat_node", chat_node)
+graph.add_edge(START, "chat_node")
+graph.add_edge("chat_node", END)
 
-# Define the edges
-graph.add_edge(START, "chat_node")  # Execution starts at "chat_node"
-graph.add_edge("chat_node", END)    # After processing, the flow ends
-
-# Compile the graph into an executable chatbot with checkpointing
 chatbot = graph.compile(checkpointer=checkpointer)
+
+def retrieve_all_threads():
+    all_threads = set()
+    for checkpoint in checkpointer.list(None):
+        all_threads.add(checkpoint.config['configurable']['thread_id'])
+
+    return list(all_threads)
 
 
 
